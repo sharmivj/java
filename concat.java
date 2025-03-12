a0540cf859b20ee8af4e5bab9b9442538fae2757
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-12 14:41:52 +0530

    sharu

---
+++ concat.java
@@ -0,0 +1,9 @@
+public class concat
+{
+    public static void main(String []args)
+    {
+        String name1="Rohini";
+        String name2="college";
+        System.out.println(name1.concat(name2));
+    }
+}
