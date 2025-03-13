1112bcc233ff1461d2fc8954b167a3a8d860cd40
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-13 11:46:26 +0530

    comp1

---
+++ comp.java
@@ -0,0 +1,17 @@
+public class comp
+{
+    public static void main(String args[])
+    {
+        String str1="sharu";
+        String str2="shamu";
+        if(str1==str2)
+        {
+            System.out.println("the string is matched");
+        }
+        else
+        {
+            System.out.println("the string is mismatched");
+        }
+        
+    }
+}
