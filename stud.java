5683c1bbdb8a322cdb253628547e4743cc085b70
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-13 10:23:30 +0530

    java pg

---
+++ stud.java
@@ -0,0 +1,14 @@
+public class stud
+{
+    public static void main(String args[])
+    {
+        String studname= "v.j.sharmi";
+        char grade='a';
+        int mark1=90,mark2=100,mark3=80;
+        System.out.println( "The  studname:" +studname);
+        System.out.println("The grade of the student:" +grade);
+        System.out.println( "the mark of the student:" +mark1);
+        System.out.println(mark2);
+        System.out.println(mark3);
+    }
+}
