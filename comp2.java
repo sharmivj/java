76b0dd4d8c12bb9e2933b53e86eae1894797d317
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-13 11:51:04 +0530

    comp2

---
+++ comp2.java
@@ -0,0 +1,14 @@
+public class comp2
+{
+    public static void main(String args[])
+    {
+        int num1=10,num2=10;
+        if(num1==num2)
+        {
+            System.out.println("they are matched");
+        }
+        else {
+            System.out.println("they are nit matched");
+        }
+    }
+}
