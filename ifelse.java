60db87e3c9076b0bd9e2a3a7b48bd170abf86464
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-13 15:17:57 +0530

    ifelse pg

---
+++ ifelse.java
@@ -0,0 +1,19 @@
+public class ifelse
+{
+    public static void main(String args[])
+    {
+        int mark=100;
+        if(mark>90)
+        {
+            System.out.println("grade:o");
+        }
+        else if(mark>80)
+        {
+            System.out.println("grade:a");
+        }
+        else if(mark>70)
+        {
+            System.out.println("grade:b");
+        }
+        }
+}
