d9fbd2904b61fa0c0346462dd9db28bf5976e70f
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-13 11:13:50 +0530

    opert

---
+++ operation.java
@@ -0,0 +1,11 @@
+public class operation
+{
+    public static void main(String args[])
+    {
+        int a=10,b=20;
+        int c=a+b;
+        int d=a*b;
+        System.out.println("the addition of a and b :" +c);
+        System.out.println("the multiplication of a and b :" +d);
+    }
+}
