3c78c71e4a67adeb833f4883ec7c623c88da53e2
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-13 13:53:27 +0530

    sawp2

---
+++ swapp2.java
@@ -0,0 +1,18 @@
+public class swapp2
+{
+    public static void main(String args[])
+    {
+        int temp,x=10,y=20;
+        System.out.println("before swapping:");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println("After swapping:");
+        temp=x;
+        x=y;
+        y=temp;
+        System.out.println(x);
+        System.out.println(y);
+        
+    
+    }
+}
