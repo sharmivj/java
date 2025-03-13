f3c9383825281e18bc338b600f1b3d31704dc53d
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-13 14:25:41 +0530

    esc

---
+++ str2.java
@@ -0,0 +1,7 @@
+public class str2{
+    public static void main(String args[])
+    {
+        String name="there is a rock,\'which is inside the hill\'";
+        System.out.println(name);
+    }
+}
