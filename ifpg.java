683476fa7c643e9e92ceccebd86e9d99f60be74d
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-13 14:38:05 +0530

    ifstatement

---
+++ ifpg.java
@@ -0,0 +1,16 @@
+public class ifpg
+{
+    public static void main(String args[])
+    {
+        int age=22;
+        if(age>18)
+        {
+            System.out.println("they are eligible to vote");
+        }
+        else
+        {
+            System.out.println("they are not eligible to vote");
+
+        }
+    }
+}
