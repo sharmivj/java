49f81512556c169373148ad595449c353d24b61e
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 10:29:39 +0530

    while

---
+++ whi.java
@@ -0,0 +1,12 @@
+public class whi
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println("the number is:" +i);
+            i++;
+        }
+    }
+}
