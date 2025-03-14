0ce944a139413216e9a7d0f7d5db936716313066
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 14:52:39 +0530

    array for loop

---
+++ arrayname.java
@@ -0,0 +1,13 @@
+public class arrayname
+{
+    public static void main(String args[])
+    {
+        String[] st ={"mango","orange","Guva","kiwi","grape"};
+        st[3]="lemon";
+        for(int i=0;i<=4;i++)
+        {
+            System.out.println(st[i]);
+
+        }
+    }
+}
