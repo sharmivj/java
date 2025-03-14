03844c9af96dbbff1d8dccc3de588a952f58568f
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 10:54:54 +0530

    javado

---
+++ do2f.java
@@ -0,0 +1,15 @@
+public class do2f
+{
+    public static void main(String args[])
+    {
+        int i=10;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=5);
+
+        
+    }
+}
