e92a05929fd02ae16e3b19198e91485cdb0a3e37
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 10:22:42 +0530

    string or not

---
+++ vowels.java
@@ -0,0 +1,15 @@
+public class vowels 
+{
+    public static void main(String args[])
+    {
+        String strname="this is a car";
+        if(strname.contains("car"))
+        {
+            System.out.println("it is a string that contain car");
+        }
+        else
+        {
+            System.out.println("it is not a string");
+        }
+    }
+}
