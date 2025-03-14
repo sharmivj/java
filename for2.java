04a5b651e107ce8c35403ed6640a08c1b4b6f3a5
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 11:14:07 +0530

    for program 2

---
+++ for2.java
@@ -0,0 +1,11 @@
+public class for2
+{
+    public static void main(String args[])
+    {
+        int i=10;
+        for(i=10;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
