89d1222f8c2a076a1af3a1911a3817c40328c782
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 13:14:36 +0530

    methodoperation

---
+++ methmul.java
@@ -0,0 +1,15 @@
+public class methmul
+{
+     static void method()
+    {
+        int a=10,b=20;
+        int c=a*b;
+        System.out.println("the product is :" +c);
+        
+
+    }
+    public static void main(String args[])
+    {
+        method();
+    }
+}
