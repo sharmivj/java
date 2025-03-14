8e11702dbc4cf86d5ac1a7755aed1ddc158e65e8
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 11:02:17 +0530

    do3

---
+++ do3.java
@@ -0,0 +1,16 @@
+public class do3
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        do
+        {
+            System.out.println("man");
+            i++;
+        
+        }
+        while(i<=5);
+
+        
+    }
+}
