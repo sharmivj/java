8ef4cd9585b5924ce43e086529408e93c1281145
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 11:48:19 +0530

    continue

---
+++ cont.java
@@ -0,0 +1,15 @@
+public class cont
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(i);
+            if(i==7)
+            {
+                continue;
+            }
+        }
+    }
+}
