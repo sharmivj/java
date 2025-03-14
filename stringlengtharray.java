f99944b0e775a8bcfd2a6c7b5a93e7f8b25f50ce
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 13:59:07 +0530

    string length array

---
+++ stringlengtharray.java
@@ -0,0 +1,14 @@
+public class stringlengtharray{
+    static void stri()
+    {
+        String[] fruit={"apple","mango","orange","kiwi","guva"};
+        System.out.println(fruit[2]);
+        System.out.println(fruit[3]);
+        System.out.println(fruit[4]);
+        System.out.println("length of the string:" +fruit.length);
+    }
+    public static void main(String args[])
+    {
+        stri();
+    }
+}
