c73b55a6102be587c49d985dc5b8908885cf45f7
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 13:52:20 +0530

    string in array

---
+++ strme.java
@@ -0,0 +1,13 @@
+public class strme{
+    static void str()
+    {
+        String[] fruit={"apple","mango","orange","kiwi","guva"};
+        System.out.println(fruit[2]);
+        System.out.println(fruit[3]);
+        System.out.println(fruit[4]);
+    }
+    public static void main(String args[])
+    {
+        str();
+    }
+}
