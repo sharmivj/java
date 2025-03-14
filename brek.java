1409f6b128077194eff0b840ca7934b4908e1f83
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 11:44:45 +0530

    break

---
+++ brek.java
@@ -0,0 +1,15 @@
+public class brek
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(i);
+            if(i==7)
+            {
+                break;
+            }
+        }
+    }
+}
