9076409df8ae38fd9513e1d1003882885e7abaf5
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-14 12:16:51 +0530

    method

---
+++ meth.java
@@ -0,0 +1,12 @@
+public class meth
+{
+     static void mymethod()
+    {
+        System.out.println("welcome");
+
+    }
+    public static void main(String args[])
+    {
+        mymethod();
+    }
+}
