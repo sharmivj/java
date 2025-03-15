545ea2819adeb2b939c5d7bff26d36889ee6c4d7
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-15 10:55:41 +0530

    product and prices

---
+++ product.java
@@ -0,0 +1,13 @@
+public class product
+{
+    public static void main(String args[])
+    {
+        String[] products={"rolls","fruits","nuts","juices","rices"};
+        int[] price={30,104,200,100,300};
+        for(int i=0;i<=4;i++)
+    
+        {
+            System.out.println(" the price of " +products[i] + ":" +price[i]);
+        }
+    }
+}
