03f2a6b425a48ef04a470399d280b3c03dbcca14
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-15 09:34:16 +0530

    inheritance'

---
+++ inheri.java
@@ -0,0 +1,15 @@
+class student
+{
+    int id;
+    String name;
+}
+class inheri
+{
+       public static void main(String args[])
+       {
+        student s1=new student();
+        s1.id=101;
+        System.out.println(" the id number is :" +s1.id);
+
+       }
+}
