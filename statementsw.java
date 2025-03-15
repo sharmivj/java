d36f39fc5ffa1e5ef71729d2e52d3f6c147d6dc0
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-15 11:15:45 +0530

    switch coffee

---
+++ statementsw.java
@@ -0,0 +1,21 @@
+public class statementsw
+{
+    public static void main(String args[])
+    {
+        int choice=2;
+        switch(choice)
+        {
+            case 1:
+                System.out.println("coffee");
+                break;
+             case 2:
+                System.out.println("cold coffee");
+                break;
+                case 3:
+                    System.out.println("hot coffee");
+                    break;
+                    default:
+                        System.out.println("not in menu");
+        }    
+    }
+}
