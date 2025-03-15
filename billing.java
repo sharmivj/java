f743014501552f53fc34b7b23f3a2ad9a4f1fd39
sharmivj <sanmi16sanmi22@gmail.com>
2025-03-15 10:26:54 +0530

    billing for customer

---
+++ billing.java
@@ -0,0 +1,11 @@
+public class billing
+{
+    public static void main(String args[])
+    {
+        int[] invoicenum={200,201,203,204,205,206};
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println("the customer invoice number is :" +invoicenum[i]);
+        }
+    }
+}
